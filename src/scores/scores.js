1d1df09f4cf814b9073562512d21ee34e49006b4
guilhermelp07 <guilpissaia@hotmail.com>
2024-05-15 23:00:28 -0300

    pequenas refatorações nas classes game.js e card.js

---
+++ src/scores/scores.js
@@ -0,0 +1,10 @@
+const scoreMapping = {
+    'A': 11,
+    'K': 10,
+    'Q': 10,
+    'J': 10
+  };
+  
+  export const calculateCardScore = (value) => {
+    return scoreMapping[value] || parseInt(value); // Se não estiver no map, retorna o próprio valor
+  };
